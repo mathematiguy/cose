339c3097141efb2f658dcea223ecdb2a92fa894e
Caleb Moses <calebjdmoses@gmail.com>
2021-03-06 15:34:26 +1300

    Use glob to grab tfrecords

--- data_scripts/calculate_data_statistics.py
+++ data_scripts/calculate_data_statistics.py
@@ -1,8 +1,8 @@
 """Calculate data statistics once and stores."""
-
 import os
 import click
 import logging
+from glob import glob
 from smartink.data.stroke_dataset import TFRecordStroke
 
 @click.command()
@@ -19,8 +19,11 @@ def main(data_dir, log_level):
     else:
       data_dir = os.path.join(data_dir, "didi_wo_text/")
 
-    TFRECORD_PATTERN = "diagrams_wo_text_20200131-?????-of-?????"
+    TFRECORD_PATTERN = "diagrams_wo_text_20200131-*-of-*"
     META_FILE = "didi_wo_text-stats-origin_abs_pos.npy"
+
+    data_paths = glob(os.path.join(data_dir, "training", TFRECORD_PATTERN))
+    logging.info("Located data files:\n{}".format('\n'.join(data_paths)))
 
     USE_POSITION = True  # Calculate statistics for pixel coordinates (i.e. absolute positions) or relative offsets (i.e., velocity).
     MAX_LENGTH = 301  # Longer or shorter strokes will be filtered out.
@@ -28,7 +31,7 @@ def main(data_dir, log_level):
 
     logging.info("Saving metadata to: {data_dir + META_FILE}")
     train_data = TFRecordStroke(
-        data_path=[os.path.join(data_dir, "training", TFRECORD_PATTERN)],
+        data_path=data_paths,
         meta_data_path=data_dir + META_FILE,
         pp_to_origin=USE_POSITION,
         pp_relative_pos=not USE_POSITION,
@@ -48,8 +51,9 @@ def main(data_dir, log_level):
         concat_t_inputs=False,
         reverse_prob=0,
         t_drop_ratio=0,
-        affine_prob=0,
+        affine_prob=0
         )
+
 
 if __name__ == "__main__":
     main()
